6d2c68f5e800a80292be8c3185175e90435dc8d9
Kondo Takeru <takechihorn@gmail.com>
2021-02-09 09:18:49 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+const viteSSRPlugin = require("vite-ssr/plugin");
+
+module.exports = {
+  plugins: [viteSSRPlugin],
+  proxy: {
+    "/api": {
+      // This is the server in `node-site` directory
+      target: "http://localhost:8080",
+    },
+  },
+};
